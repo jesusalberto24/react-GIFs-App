772afcd4ba991ae5123ba3b2a4d425f3c1786d7b
Jesus Mogollon <beto-barca10@hotmail.com>
2020-10-10 18:12:04 -0400

    docs agregado

---
+++ docs/precache-manifest.b3ae58d5e1b83b924b3971a630e94096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da6c96e3eeb05feb7f1e6cde853fc22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97dc8291a7651cadf9d6",
+    "url": "/static/css/main.a714e651.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97dc8291a7651cadf9d6",
+    "url": "/static/js/main.4b7db93c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
